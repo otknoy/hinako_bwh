3b1e56b1df36e4d7b7e9408241c0055f21fe9fdd
otknoy <otknoy@gmail.com>
2015-06-07 17:55:01 +0900

    Implement Hinako API

--- hinako_api/hinako_api.py
+++ hinako_api/hinako_api.py
@@ -4,11 +4,27 @@ from bottle import route, run, request
 
 from hinako import Hinako
 
-port = '/dev/cu.usbmodemfa131'
+port = '/dev/cu.usbmodemfd121'
 hinako = Hinako(port, 'd:3:s', 'd:4:s', 'd:5:s')
 
-
 # Hinako API
+@route('/hinako', method='GET')
+def index():
+    query = request.query
+
+    print query
+
+    b = int(query.b)
+    w = int(query.w)
+    h = int(query.h)
+
+    hinako.set_bust(b)
+    hinako.set_waist(w)
+    hinako.set_hip(h)
+
+    return 'hinako!'
+
+
 @route('/bust/<size:int>')
 def bust(size):
     hinako.set_bust(size)
